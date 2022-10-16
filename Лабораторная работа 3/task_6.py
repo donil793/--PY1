ca4ed92c2f0bd530277c917a78ff0c1a46309f26
donil793 <115420622+donil793@users.noreply.github.com>
2022-10-17 01:14:22 +0500

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task_6.py
@@ -0,0 +1,9 @@
+list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+max_ = max(list_numbers)
+len_=len(list_numbers)
+i=0
+while list_numbers[i]!=max_:
+    i=i+1
+
+list_numbers[i], list_numbers[-1]=list_numbers[-1], list_numbers[i]
+print(list_numbers)
